525dc03b987887f088585771d8c96ce2b24a3416
lazyingman <106133469+lazyingman@users.noreply.github.com>
2023-05-17 14:00:09 +0000

    更新了图片加载失败和网站图标 Updated By Github Actions

--- js/random.js
+++ js/random.js
@@ -1,4 +1,4 @@
-var posts = ["posts/697cb99d.html","posts/4a17b156.html"];
+var posts = ["posts/697cb99d.html","posts/4a17b156.html","posts/fcbdffd2.html","posts/e2a091a6.html","posts/da523892.html","posts/20230502a.html"];
 function toRandomPost() {
     pjax.loadUrl('/' + posts[Math.floor(Math.random() * posts.length)]);
 }
