444186f952065fac0f50ef19715efc990d893606
lazyingman <106133469+lazyingman@users.noreply.github.com>
2023-06-30 13:31:14 +0000

    更新了友链和文章，修改了隐私协议 Updated By Github Actions

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,6 @@
             var json = await response.json();
 
             var ip = json.ip;
-            var continent = json.data.continent;
             var country = json.data.country;
             var prov = json.data.prov;
             var city = json.data.city;
@@ -14,14 +13,16 @@
             var isp = json.data.isp;
 
             document.getElementById("userAgentIp").innerHTML = ip;
-            document.getElementById("userAgentState").innerHTML = continent;
+            // document.getElementById("userAgentState").innerHTML = continent;
             document.getElementById("userAgentCountry").innerHTML = country;
             document.getElementById("userAgentProv").innerHTML = prov;
             document.getElementById("userAgentCity").innerHTML = city;
-            document.getElementById("userAgentDistrict").innerHTML = district;
+            // document.getElementById("userAgentDistrict").innerHTML = district;
             document.getElementById("userAgentISP").innerHTML = isp;
+            
 
             var uaInfo = navigator.userAgent;
+
             document.getElementById("userAgentDevice").innerHTML = uaInfo;
         } catch (error) {
             // console.error("An error occurred while fetching IP info:", error);
